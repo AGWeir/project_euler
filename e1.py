907b3d756fea489695e8a25ddd5b3bad280359db
AGWeir <AWGWeir@gmail.com>
2016-12-02 15:02:39 +0000

    Update e1.py

--- e1.py
+++ e1.py
@@ -26,3 +26,7 @@ def euler1better(n):
         elif i % 5 == 0:
             s += i
     return s
+
+#Alternative method same performance
+def euler1alt(n):
+    return sum([i for i in range(n) if (i % 3 == 0 or i % 5 == 0)])
